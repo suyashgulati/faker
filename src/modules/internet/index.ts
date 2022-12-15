156a540e96b03983392e788be6bedec31d6d5f4e
xDivisionByZerox <leyla.jaehnig@gmx.de>
2022-12-15 23:06:29 +0100

    refactor(internet): move password implementation into module

--- src/modules/internet/index.ts
+++ src/modules/internet/index.ts
@@ -1,6 +1,5 @@
 import type { Faker } from '../..';
 import { charMapping } from './char-mappings';
-import { passwordFnFactory } from './password';
 import * as random_ua from './user-agent';
 
 export type EmojiType =
@@ -23,6 +22,8 @@ export type HTTPStatusCodeType =
   | 'redirection';
 
 export type HTTPProtocolType = 'http' | 'https';
+
+export type PasswordMode = 'secure' | 'simple';
 
 /**
  * Module to generate internet related entries.
@@ -499,7 +500,180 @@ export class InternetModule {
     return mac;
   }
 
-  password = passwordFnFactory(this.faker);
+  /**
+   * Generates a random password.
+   *
+   * @param mode The mode in which the password will be generated.
+   * - 'secure': A string with a length between 24 and 64
+   * and all character types required.
+   * - 'simple': A string with a length between 4 and 8,
+   * where characters can be upper **OR** lowercase, and numbers.
+   *
+   * Defaults to 'secure'.
+   */
+  password(mode: PasswordMode): string;
+  /**
+   * Generates a random password.
+   *
+   * @param options An options object.
+   * @param options.length The specific length of the password.
+   * @param options.includeLowercase Whether lowercase letters should be included.
+   * If a number is provided the final result will have at least this many lowercase letters.
+   * @param options.includeNumber Whether numbers should be included.
+   * If a number is provided the final result will have at least this many numeric characters.
+   * @param options.includeSymbol Whether symbols should be included.
+   * If a number is provided the final result have will at least this many special characters.
+   * @param options.includeUppercase Whether uppercase letters should be included.
+   * If a number is provided the final result will have at least this many uppercase letters.
+   */
+  password(options: {
+    /**
+     * The specific length of the password.
+     */
+    length: number;
+    /**
+     * Whether lowercase letters should be included.
+     * If a number is provided the final result will have at least this many lowercase letters.
+     */
+    includeLowercase: boolean | number;
+    /**
+     * Whether numbers should be included.
+     * If a number is provided the final result will have at least this many numeric characters.
+     */
+    includeNumber: boolean | number;
+    /**
+     * Whether symbols should be included.
+     * If a number is provided the final result have will at least this many special characters.
+     */
+    includeSymbol: boolean | number;
+    /**
+     * Whether uppercase letters should be included.
+     * If a number is provided the final result will have at least this many uppercase letters.
+     */
+    includeUppercase: boolean | number;
+  }): string;
+  /**
+   * Generates a random password.
+   *
+   * @param options A password generation mode or an options opbject.
+   * @param options.length The specific length of the password.
+   * @param options.includeLowercase Whether lowercase letters should be included.
+   * If a number is provided the final result will have at least this many lowercase letters.
+   * @param options.includeNumber Whether numbers should be included.
+   * If a number is provided the final result will have at least this many numeric characters.
+   * @param options.includeSymbol Whether symbols should be included.
+   * If a number is provided the final result have will at least this many special characters.
+   * @param options.includeUppercase Whether uppercase letters should be included.
+   * If a number is provided the final result will have at least this many uppercase letters.
+   */
+  password(
+    options:
+      | PasswordMode
+      | {
+          /**
+           * The specific length of the password.
+           */
+          length: number;
+          /**
+           * Whether lowercase letters should be included.
+           * If a number is provided the final result will have at least this many lowercase letters.
+           */
+          includeLowercase: boolean | number;
+          /**
+           * Whether numbers should be included.
+           * If a number is provided the final result will have at least this many numeric characters.
+           */
+          includeNumber: boolean | number;
+          /**
+           * Whether symbols should be included.
+           * If a number is provided the final result have will at least this many special characters.
+           */
+          includeSymbol: boolean | number;
+          /**
+           * Whether uppercase letters should be included.
+           * If a number is provided the final result will have at least this many uppercase letters.
+           */
+          includeUppercase: boolean | number;
+        } = 'secure'
+  ): string {
+    if (typeof options === 'string') {
+      switch (options) {
+        case 'secure': {
+          options = {
+            includeLowercase: true,
+            includeNumber: true,
+            includeSymbol: true,
+            includeUppercase: true,
+            length: this.faker.number.int({
+              min: 24,
+              max: 64,
+            }),
+          };
+          break;
+        }
+        case 'simple':
+        default: {
+          const useLower = this.faker.datatype.boolean();
+          options = {
+            includeLowercase: useLower,
+            includeNumber: true,
+            includeSymbol: false,
+            includeUppercase: !useLower,
+            length: this.faker.number.int({
+              min: 4,
+              max: 8,
+            }),
+          };
+          break;
+        }
+      }
+    }
+
+    const getCharCountFromOptions = (opt: boolean | number) => {
+      if (typeof opt === 'boolean') {
+        return opt ? 1 : 0;
+      } else {
+        return opt >= 0 ? opt : 0;
+      }
+    };
+
+    const charGroups = [
+      {
+        requireCount: getCharCountFromOptions(options.includeLowercase),
+        generatorFn: () => this.faker.string.alpha({ casing: 'lower' }),
+      },
+      {
+        requireCount: getCharCountFromOptions(options.includeUppercase),
+        generatorFn: () => this.faker.string.alpha({ casing: 'upper' }),
+      },
+      {
+        requireCount: getCharCountFromOptions(options.includeNumber),
+        generatorFn: () => this.faker.string.numeric(),
+      },
+      {
+        requireCount: getCharCountFromOptions(options.includeSymbol),
+        generatorFn: () => this.faker.string.special(),
+      },
+    ];
+
+    const chars: string[] = [];
+    for (const groupOptions of charGroups) {
+      const { generatorFn } = groupOptions;
+      let { requireCount } = groupOptions;
+      while (requireCount > 0) {
+        chars.push(generatorFn());
+        requireCount--;
+      }
+    }
+
+    while (chars.length < options.length) {
+      const groupIndex = this.faker.number.int(charGroups.length - 1);
+      const nextChar = charGroups[groupIndex].generatorFn();
+      chars.push(nextChar);
+    }
+
+    return this.faker.helpers.shuffle(chars).join('');
+  }
 
   /**
    * Generates a random emoji.
